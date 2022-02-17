b1d3a84de974753faa5ad7aca48b29e74157562e
Leonardo Pottmayer <pottmayerleonardo@gmail.com>
2022-02-17 15:36:57 -0300

    Commit 17/02/2022 - 1

---
+++ index.js
@@ -0,0 +1,12 @@
+//Function to calculate my age, and update on my page
+function calculateAge() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var myAge = year - 2003;
+    console.log(myAge);
+
+    var text = document.querySelector(".about-description");  
+    text.innerHTML = "Meu nome é Leonardo Gian Pottmayer, tenho " + myAge + " anos, nascido em Pomerode - SC. Comecei a estudar programação há algum tempo. Hoje estou cursando Ciência da Computação na Universidade Regional de Blumenau  - FURB, embora prefira aprender por conta própria, pesquisando várias tecnologias para maximizar meu conhecimento";
+}
+
+calculateAge();
--- js/index.js
+++
@@ -1,9 +0,0 @@
-//Function to calculate my age, and update on my page
-function calculateAge() {
-    var date = new Date();
-    var year = date.getFullYear();
-    var myAge = year - 2003;
-    console.log(myAge);
-
-    var text = document.querySelector(".about-description").innerHTML = "Meu nome é Leonardo Gian Pottmayer, tenho " + myAge + " anos, nascido em Pomerode - SC. Comecei a estudar programação há algum tempo. Hoje estou cursando Ciência da Computação na Universidade Regional de Blumenau  - FURB, embora prefira aprender por conta própria, pesquisando várias tecnologias para maximizar meu conhecimento"
-}
